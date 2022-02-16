36671eee2d9a9ee776ee097f1d5b42edfe5832ba
user <user@WIN-C40745DRNC4>
2022-02-16 12:43:46 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,29 @@
+﻿/*
+by:senb1
+*/
+#include <iostream>
+
+using namespace std;
+
+#define ll long long
+
+ll a, b, c, d, f;
+
+ll binpow(ll a, ll n) {
+    if (n == 0) return 1;
+    if (n % 2) return binpow(a, n - 1) * a % f;
+    ll b = binpow(a, n / 2) % f;
+    return b * b % f;
+}
+
+ll gcd(ll a, ll b) {
+    return b ? gcd(b, a % b) : a;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    cin >> a >> b >> c >> d >> f;
+    ll q = gcd(a, c);
+    ll ans = binpow(q, max(b, d));
+    cout << ans << endl;
+}
